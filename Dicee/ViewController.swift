8e5a435191cdcfad390398a3995de36b3ae2ce28
mikulicJurica <jurica.mikulic18@gmail.com>
2022-10-31 00:01:41 +0100

    UI config

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -1,19 +1,23 @@
-//
-//  ViewController.swift
-//  Dicee
-//
-//  Created by Jurica Mikulic on 29.10.2022..
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    
+    private var diceImages = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        leftDiceImageView.image = diceImages[Int.random(in: 0...5)]
+        rightDiceImageView.image = diceImages[Int.random(in: 0...5)]
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        leftDiceImageView.image = diceImages[Int.random(in: 0...5)]
+        rightDiceImageView.image = diceImages[Int.random(in: 0...5)]
+    }
 }
 
